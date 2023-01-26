00423f74f8404f57f512bdd0d18dfe7b98c097d4
Kieran <kieran.webdevelopment@gmail.com>
2023-01-26 19:02:08 +0000

    basic js functionality completed

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,12 @@
-// import data.json file in js file
-// import jsonData from './' assert { type: 'json' };
-// const userData = jsonData;
-// console.log(userData);
+const activities = document.querySelectorAll('.activity-title');
+const timeAmount = document.querySelectorAll('.time-amount');
+const timeLastWeek = document.querySelectorAll('.previous-time-amount');
+console.log(activities);
+// Buttons
+const btnDaily = document.querySelector('.daily');
+const btnWeekly = document.querySelector('.weekly');
+const btnMonthly = document.querySelector('.monthly');
+
 async function getData() {
   try {
     const response = await fetch('./data.json');
@@ -13,3 +18,20 @@ async function getData() {
 }
 const userData = await getData();
 console.log(userData);
+
+// Daily
+
+function displayHours(timeamount) {
+  timeAmount.forEach((time, index) => {
+    time.textContent = `${userData[index].timeframes[timeamount].current}hrs`;
+  });
+  timeLastWeek.forEach((time, index) => {
+    time.textContent = `Last Week - ${userData[index].timeframes[timeamount].previous}hrs`;
+  });
+}
+
+// Event Listeners
+
+btnDaily.addEventListener('click', () => displayHours('daily'));
+btnWeekly.addEventListener('click', () => displayHours('weekly'));
+btnMonthly.addEventListener('click', () => displayHours('monthly'));
