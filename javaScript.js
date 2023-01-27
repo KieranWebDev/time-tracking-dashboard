71a1a13eaeb4ff9079b2b06b3f4c7e508ecfb538
Kieran <kieran.webdevelopment@gmail.com>
2023-01-27 09:18:02 +0000

    added btn color functionality with js instead of css

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,4 @@
+'use strict';
 const activities = document.querySelectorAll('.activity-title');
 const timeAmount = document.querySelectorAll('.time-amount');
 const timeLastWeek = document.querySelectorAll('.previous-time-amount');
@@ -20,14 +21,26 @@ async function getData() {
 const userData = await getData();
 console.log(userData);
 
-// Daily
+function btnColor(timeframe) {
+  timeBtns.forEach((btn) => {
+    if (btn.classList.contains(timeframe)) {
+      btn.style.color = 'white';
+    } else {
+      btn.style.color = '#7078c9';
+    }
+  });
+}
 
-function displayHours(timeamount) {
+function displayHours(timeframe) {
+  btnColor(timeframe);
+  activities.forEach((activity, index) => {
+    activity.textContent = userData[index].title;
+  });
   timeAmount.forEach((time, index) => {
-    time.textContent = `${userData[index].timeframes[timeamount].current}hrs`;
+    time.textContent = `${userData[index].timeframes[timeframe].current}hrs`;
   });
   timeLastWeek.forEach((time, index) => {
-    time.textContent = `Last Week - ${userData[index].timeframes[timeamount].previous}hrs`;
+    time.textContent = `Last Week - ${userData[index].timeframes[timeframe].previous}hrs`;
   });
 }
 displayHours('daily');
